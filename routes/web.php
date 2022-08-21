264e5fbcf27ccdd5f94def78a92eeae65222180d
faizalvian <faizalvian97@gmail.com>
2022-08-21 22:44:00 +0700

    konversi html ke blade

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/categories', 'CategoryController@index')->name('categories');
+Route::get('/detail/{id}', 'DetailController@index')->name('detail');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
